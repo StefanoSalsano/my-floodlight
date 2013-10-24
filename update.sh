a8124ef25e7e7a828c2951ff386feda19a604def
Pier Luigi Ventre <pierventre@hotmail.com>
2013-10-24 17:29:11 +0200

    First Commmit For New Branch - ICOFI=It Caches Only Far Items - added my script

---
+++ update.sh
@@ -0,0 +1,3 @@
+wget https://www.dropbox.com/s/u4xjml3z18vyc2l/conetcontroller.conf -O ./conetcontroller.conf
+wget https://www.dropbox.com/s/38tdqcbqbk85o6h/ConetModule.java -O ./src/main/java/local/conet/ConetModule.java
+ant
